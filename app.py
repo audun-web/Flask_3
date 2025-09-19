3b05fce0647290cefbdc3ff9540ea8195572b684
audun-web <audun@midtmeck.com>
2025-09-19 14:58:47 +0200

    La til en om meg side på /meg med enkle html tags

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,4 +7,11 @@ app = Flask(__name__)
 def home():
     return '''
     <h1>Hjemside</h1>
+    '''
+
+@app.rouge('/meg')
+def meg():
+    return '''
+    <h1>Om meg</h1>
+    <p>Jeg er meg</p>
     '''
